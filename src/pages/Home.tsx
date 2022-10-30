dc5e1da4aefe23cb805dc0799341ae12128f5a70
truejojo <johannesveh@yahoo.de>
2022-10-30 13:19:23 +0100

    created input and fetch data from api.tvmaze.com

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,43 @@
-import React from "react";
+import { useState } from "react";
 import MainPageLayout from "../components/MainPageLayout";
 
 const Home = () => {
-  return <MainPageLayout>home</MainPageLayout>;
+  const [input, setInput] = useState<string>("");
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setInput(event.target.value);
+
+  const onSearch = () => {
+    const catchData = async () => {
+      const response = await fetch(
+        `https://api.tvmaze.com/search/shows?q=${input}`
+      );
+      const jsonData = await response.json();
+      console.log(jsonData);
+    };
+    catchData();
+    setInput("");
+  };
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      onSearch();
+    }
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        value={input}
+        onChange={handleOnChange}
+        onKeyDown={onKeyDown}
+      />
+      <button type="button" onClick={onSearch}>
+        Search...
+      </button>
+    </MainPageLayout>
+  );
 };
 
 export default Home;
