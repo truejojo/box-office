64feddb082ceb30bc4527eddb3baadc92833e8be
truejojo <johannesveh@yahoo.de>
2022-11-06 14:29:30 +0100

    added loading and error message to the show page

--- src/pages/Show.tsx
+++ src/pages/Show.tsx
@@ -4,16 +4,39 @@ import { apiGet } from "../misc/config";
 
 const Show = () => {
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      setShow(results);
-    });
+    let isMounted = true;
+
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then((results) => {
+        if (isMounted) {
+          setShow(results);
+          setIsLoading(false);
+        }
+      })
+      .catch((err) => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
 
-  console.log(`show: ${show}`);
-  
+  if (isLoading) {
+    return <div>Data is being loaded</div>;
+  }
+
+  if (error) {
+    return <div>Error occured: {error}</div>;
+  }
 
   return <div>Show: {id}</div>;
 };
