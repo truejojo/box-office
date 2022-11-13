57e995c34e99c8b8366b5d5e7ec9f6fca88fb3f3
truejojo <johannesveh@yahoo.de>
2022-11-13 16:48:09 +0100

    created custom hooks for shows

--- src/components/show/Cast.styled.ts
+++ src/components/show/Cast.styled.ts
@@ -5,13 +5,13 @@ export const CastList = styled.div`
   flex-wrap: wrap;
 
   .cast-item {
-    flex: 1 0 50%;
+    flex: 1 0 100%;
     display: flex;
     margin-bottom: 20px;
     align-items: center;
 
-    @media only screen and (max-width: 768px) {
-      flex: 1 0 100%;
+    @media only screen and (mim-width: 768px) {
+      flex: 1 0 50%;
     }
   }
 
---
+++ src/misc/custom-hooks.tsx
@@ -0,0 +1,57 @@
+import { useReducer, useEffect } from "react";
+
+enum ActionTypes {
+  ADD,
+  REMOVE,
+}
+
+type ACTIONS =
+  | {
+      type: ActionTypes.ADD;
+      showId: number;
+    }
+  | {
+      type: ActionTypes.REMOVE;
+      showId: number;
+    };
+
+const initialState: number[] = [];
+
+// const showReducer = (prevState: number[], action: ACTIONS) => {
+const showReducer = (prevState: typeof initialState = [], action: ACTIONS) => {
+  switch (action.type) {
+    case ActionTypes.ADD: {
+      return [...prevState, action.showId];
+    }
+    case ActionTypes.REMOVE: {
+      return prevState.filter((showId) => showId !== action.showId);
+    }
+    default:
+      return prevState;
+  }
+};
+
+const userPersistedReducer = (
+  reducer: (prevState: number[], action: ACTIONS) => number[],
+  initialState: number[],
+  key: string
+) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [state, dispatch] = useReducer(showReducer, initialState, (initial) => {
+    const persisted = localStorage.getItem(key);
+
+    return persisted ? JSON.parse(persisted) : initial;
+  });
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [state, key]);
+
+  return [state, dispatch];
+};
+
+export const useShows = (key: string = "shows") => {
+  return userPersistedReducer(showReducer, initialState, key);
+  // return userPersistedReducer(key);
+};
